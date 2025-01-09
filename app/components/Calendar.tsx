804e287b6d402e61a24ed035b2582a778a8ad6f1
Jesper <jthabo@gmail.com>
2025-01-09 07:32:40 +0100

    adding some fullcalendar options

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -10,13 +10,12 @@ export default function Calendar() {
   return (
     <FullCalendar
       plugins={[interActionPlugin, dayGridPlugin, timeGridPlugin]}
-      initialView='dayGridMonth'
+      initialView="dayGridMonth"
       headerToolbar={{
-        left: "prev,next",
-        center: "title",
-        right: "dayGridMonth,timeGridWeek,timeGridDay",
+        left: "title",
+        right: "prev,next,dayGridMonth,timeGridWeek,timeGridDay",
       }}
-      weekNumberCalculation='ISO'
+      weekNumberCalculation="ISO"
       weekNumbers={true}
       weekNumberContent={(args) => args.num}
       slotLabelFormat={{
@@ -29,9 +28,44 @@ export default function Calendar() {
         minute: "2-digit",
         hour12: false,
       }}
-      slotDuration='00:30:00'
-      events={{}}
+      slotMinTime={"06:00"}
+      slotMaxTime={"23:45"}
+      slotDuration="00:30:00"
+      events={[
+        {
+          title: "Sofie",
+          start: "2025-01-10T14:00:00",
+          end: "2025-01-10T16:00:00",
+          extendedProps: {
+            roomId: "Room B",
+          },
+        },
+      ]}
+      eventContent={(arg) => {
+        // Destructure extendedProps inside the callback
+        const { user, roomId } = arg.event.extendedProps;
+        const startTime = arg.event.start!.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+
+        const endTime = arg.event.end!.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+
+        return (
+          <div>
+            <b>{arg.event.title}</b> {`${startTime} - ${endTime}`}
+          </div>
+        );
+      }}
       //dateClick={}
+      dateClick={(info) => {
+        console.log("Clicked on date:", info.dateStr);
+      }}
     />
   );
 }
